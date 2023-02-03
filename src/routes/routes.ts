6399941d2a78a5ccc83c1cf24fc723a050f5c64c
Hundraw <chauvel.dimitri@posteo.net>
2023-02-03 10:41:31 +0100

    Improve typescript in components

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw, type Router } from 'vue-router'
 import TrendingView from '../views/TrendingView.vue'
 import SubscriptionsView from '../views/SubscriptionsView.vue'
 
-const routes = [
+const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'Trending',
     component: TrendingView
   },
   {
-    path: '/subscriptions/:id',
+    path: '/subscriptions/:name',
     name: 'Subscriptions',
     component: SubscriptionsView
   }
 ]
 
-const router = createRouter({
+const router: Router = createRouter({
   history: createWebHistory(),
   routes
 })
